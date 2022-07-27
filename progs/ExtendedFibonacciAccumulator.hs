b2f4e4aa996845672adb5da7b8139d7719bf6efa
Oleksandr Dovhal <gribnik7676@gmail.com>
2022-07-28 01:42:03 +0300

    First haskell (λ) programm: Fibonacci tail recursion

---
+++ progs/ExtendedFibonacciAccumulator.hs
@@ -0,0 +1,16 @@
+module ExtendedFibonacciAccumulator where
+
+-- Functional of fibonacci
+fib' :: Integer -> Integer -> Integer -> Integer
+fib' a b n | n == 0 = a
+           | otherwise = fib' b (a+b) (n-1)
+
+{- fibonacci
+ - 
+ - param: n - sequence number of fibonacci
+ -}
+fibonacci :: Integer -> Integer
+fibonacci n | n >= 0 = fib' 0 1 n
+          | otherwise = if odd n
+            then fib' 0 1 $ abs n
+            else - (fib' 0 1 $ abs n)
